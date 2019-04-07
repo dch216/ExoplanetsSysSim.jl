7ee05493de1907f86527494d6ef4c1c56964529f
dch216 <dch216@psu.edu>
2019-04-07 15:45:23 -0400

    Rename Data Files to Work with Existing Simulations Params

--- data/replace_koi_mcmc.jl
+++ data/replace_koi_mcmc.jl
@@ -22,7 +22,7 @@ function interp_quantile_list(x::AbstractArray{T,1},q::Real;
 end
 
 mcmc_filename = "dr25_koi_mcmc_quant.csv"
-koi_filename = "q1_q17_dr25_koi.csv"
+koi_filename = "q1_q17_dr25_koi_original.csv"
 mcmc_df = CSV.read(mcmc_filename)
 tmp_koi_cat = readlines(koi_filename)
 tmp_ind = 1
@@ -41,21 +41,38 @@ df[:koi_duration_cat] = map(x -> x, df[:koi_duration])
 df[:koi_duration_err1_cat] = map(x -> x, df[:koi_duration_err1])
 df[:koi_duration_err2_cat] = map(x -> x, df[:koi_duration_err2])
 
+num_quantiles = 99
+quantile_list = range(1/(num_quantiles+1), 1/(num_quantiles+1), num_quantiles)
+q_hi = 0.5+0.34134
+q_mid = 0.5
+q_lo = 0.5-0.34134
+
 for j in 1:length(df[:kepoi_name])
-if !ismissing(df[j, :depth_mean]) && df[j, :depth_mean] >= 0
-depth_quantiles = Array{Float64, 1}()
-duration_quantiles = Array{Float64, 1}()
-for i in 1:num_quantiles
-push!(depth_quantiles, df[j, Symbol("depth_q" * string(i))]*1e6)
-push!(duration_quantiles, df[j, Symbol("duration_q" * string(i))]*24)
-end
-df[j, :koi_depth] = interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
-df[j, :koi_depth_err1] =  interp_quantile_list(depth_quantiles,q_hi,quantile_list=quantile_list) - interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
-df[j, :koi_depth_err2] =  interp_quantile_list(depth_quantiles,q_lo,quantile_list=quantile_list) - interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
-df[j, :koi_duration] =  interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
-df[j, :koi_duration_err1] = interp_quantile_list(duration_quantiles,q_hi,quantile_list=quantile_list) - interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
-df[j, :koi_duration_err2] = interp_quantile_list(duration_quantiles,q_lo,quantile_list=quantile_list)- interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
-end
+    if !ismissing(df[j, :depth_mean]) && df[j, :depth_mean] >= 0
+        depth_quantiles = Array{Float64, 1}()
+        duration_quantiles = Array{Float64, 1}()
+        for i in 1:num_quantiles
+            push!(depth_quantiles, df[j, Symbol("depth_q" * string(i))]*1e6)
+            push!(duration_quantiles, df[j, Symbol("duration_q" * string(i))]*24)
+        end
+        df[j, :koi_depth] = interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
+        df[j, :koi_depth_err1] =  interp_quantile_list(depth_quantiles,q_hi,quantile_list=quantile_list) - interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
+        df[j, :koi_depth_err2] =  interp_quantile_list(depth_quantiles,q_lo,quantile_list=quantile_list) - interp_quantile_list(depth_quantiles,q_mid,quantile_list=quantile_list)
+        df[j, :koi_duration] =  interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
+        df[j, :koi_duration_err1] = interp_quantile_list(duration_quantiles,q_hi,quantile_list=quantile_list) - interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
+        df[j, :koi_duration_err2] = interp_quantile_list(duration_quantiles,q_lo,quantile_list=quantile_list)- interp_quantile_list(duration_quantiles,q_mid,quantile_list=quantile_list)
+    end
 end
 
-CSV.write("q1_q17_dr25_koi_mcmc.csv", df)
+delete!(df, :depth_mean)
+delete!(df, :depth_std)
+delete!(df, :duration_mean)
+delete!(df, :duration_std)
+delete!(df, :radr_gt1_cnt)
+delete!(df, :b_gt1_cnt)
+for i in 1:num_quantiles
+    delete!(df, Symbol("depth_q" * string(i)))
+    delete!(df, Symbol("duration_q" * string(i)))
+end
+
+CSV.write("q1_q17_dr25_koi.csv", df)
