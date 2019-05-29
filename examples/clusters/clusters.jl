db214b5e3974fccfb0a4045821f235c677136f99
dch216 <dch216@psu.edu>
2019-05-29 12:28:49 -0400

    Cleanup of nonrelevant files for Hsu et al. (2019) release

--- examples/clusters/clusters.jl
+++
@@ -1,9 +0,0 @@
-using ExoplanetsSysSim
-
-#include("param_custom.jl")
-include("param_common.jl")
-include("model.jl")
-include("summary_stats.jl")
-include("distance.jl")
-include("stellar_catalog.jl")
-
